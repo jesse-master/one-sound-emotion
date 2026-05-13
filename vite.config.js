322420411737f6f7f190c0d14dd420ab552d23c8
ctmappuber <entreondaseemocoes@gmail.com>
2026-05-13 14:19:13 -0300

    Transforma app em PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'One Sound Emotion',
+        short_name: 'One Sound',
+        description: 'App oficial para ouvir minhas músicas online.',
+        theme_color: '#00b894',
+        background_color: '#101010',
+        display: 'standalone',
+        start_url: '/one-sound-emotion/',
+        scope: '/one-sound-emotion/',
+        icons: [
+          {
+            src: '/one-sound-emotion/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/one-sound-emotion/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: '/one-sound-emotion/',
 })
